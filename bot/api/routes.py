686fa6918170cf3e76f2b3b87576b8f85ec417a1
Szchiji <118823636+Szchiji@users.noreply.github.com>
2026-09-10 03:41:26 +0800

    feat(ux): register routes_media

--- bot/api/routes.py
+++ bot/api/routes.py
@@ -2,5 +2,6 @@
 from bot.api.routes_core import router  # noqa: F401
 import bot.api.routes_posts  # noqa: F401
 import bot.api.routes_admin  # noqa: F401
+import bot.api.routes_media  # noqa: F401
 
 __all__ = ["router"]
