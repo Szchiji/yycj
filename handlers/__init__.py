bd35f892e9b5a2c2024030645fb6ee3236f699e9
Szchiji <118823636+Szchiji@users.noreply.github.com>
2026-09-09 16:03:05 +0800

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import menu, checkin, member, autoreply, schedule
-
-def register(application):
-    menu.register(application)
-    checkin.register(application)
-    member.register(application)
-    autoreply.register(application)
-    schedule.register(application)
