f783034d3a9249a58cb306b4d371085dcf4d8854
Szchiji <118823636+Szchiji@users.noreply.github.com>
2026-09-25 19:16:07 +0800

    disable competing live-refresh fetch wrap

--- miniapp/live-refresh.js
+++ miniapp/live-refresh.js
@@ -1,9 +1,1 @@
-(() => {
-  window.yycjMarkDirty = function () {
-    try { localStorage.setItem("yycj_bust", String(Date.now())); } catch (e) {}
-  };
-  window.yycjRefreshViews = function () {
-    if (typeof window.__yycjPaintHomeNow === "function") window.__yycjPaintHomeNow();
-    else if (typeof window.__yycjReloadHome === "function") window.__yycjReloadHome();
-  };
-})();
+/* disabled */
